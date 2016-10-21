8042d0f7d957cefefcc838a8b56ae3904962bc1e
bakuzan <imannoyed4days@hotmail.co.uk>
2016-10-21 12:04:38 +0100

    ontv : added onPlayButton, needs tests.

--- open-new-tab-video/open-new-tab-video.user.js
+++ open-new-tab-video/open-new-tab-video.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Open new tab video
 // @namespace    http://github.com/bakuzan/user-scripts
-// @version      0.0.4
+// @version      0.0.6
 // @description  Allow you to open a video in a new tab.
 // @author       Bakuzan
 // @include      http*
@@ -19,9 +19,10 @@
 	
     //As a start handle html5 videos.
     var NEW_TAB_BUTTON_ID_PREFIX = 'userscript-ontv-button-',
-		NEW_TAB_BUTTON_CLASS = 'userscript-ontv-button',
+	    NEW_TAB_BUTTON_CLASS = 'userscript-ontv-button',
         NEW_TAB_CONTAINER_ID_PREFIX = 'userscript-ontv-container-',
-		NEW_TAB_CONTAINER_CLASS = 'userscript-ontv-container',
+	    NEW_TAB_CONTAINER_CLASS = 'userscript-ontv-container',
+	    onPlayButton = document.createElement('input'),
         videos = document.getElementsByTagName('video');
     
     if(!videos.length) return;
@@ -30,12 +31,27 @@
         child.parentNode.replaceChild(newParent, child);
         newParent.appendChild(child);
     }
+	
+	function onPlayOpenInNewTab(event) {
+		var target = event.target;
+		window.open(target.getAttribute('video-link'), '_blank');
+	}
     
     function openVideoInNewTab(event) {
         var target = event.target,
 			video = target.parent.getElementsByTagName('video')[0];
         window.open(video.getAttribute('src'), '_blank');
     }
+	
+	function showOnPlayButton(event) {
+		var target = event.target;
+		console.log('played target: ', target, onPlayButton);
+		onPlayButton.style.display = 'block';
+		onPlayButton.setAttribute('video-link', target.getAttribute('src'))
+		setTimeOut(function() {
+			onPlayButton.style.display = 'none';
+		}, 5000);
+	}
     
     function createOpenInNewTabButton(index, video) {
         var container = document.createElement('div');
@@ -52,10 +68,18 @@
         container.appendChild(newTabButton);
         return container;
     }
-    
-    for(var i = 0, length = videos.length; i < length; i++) {
-        var video = videos[i],
-            openInNewTabButton = createOpenInNewTabButton(i, video);
-        wrapElementWithNewParent(openInNewTabButton, video);
-    }
+	
+    (function() {
+		onPlayButton.id = `${NEW_TAB_BUTTON_ID_PREFIX.slice(0, -1)}`;
+		onPlayButton.type = 'button';
+		onPlayButton.value = 'Open video in new tab?';
+		onPlayButton.addEventListener('click', onPlayOpenInNewTab);
+		
+		for(var i = 0, length = videos.length; i < length; i++) {
+			var video = videos[i],
+				openInNewTabButton = createOpenInNewTabButton(i, video);
+			video.addEventListener('play', showOnPlayButton);
+			wrapElementWithNewParent(openInNewTabButton, video);
+		}
+	})();
 })();
