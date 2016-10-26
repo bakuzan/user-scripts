8d29060c3d953e4045aef16669acb4e628e2d314
bakuzan <imannoyed4days@hotmail.co.uk>
2016-10-26 21:52:37 +0100

    ...urh

--- open-new-tab-video/open-new-tab-video.user.js
+++ open-new-tab-video/open-new-tab-video.user.js
@@ -96,15 +96,45 @@
 			}
 			window.addEventListener('message', receiveMessageFromFrame, false);
 		})();
-	} else {
-		
-		(function(open) {
-			window.XMLHttpRequest.prototype.open = function() {
-				this.addEventListener('readystatechange', function() {
-					console.log('xmlhttprequest open: ', this, this.getResponseHeader('content-type'), arguments);
-				}, false);
-				open.apply(this, arguments);
+	} else {	
+		(function(XHR) {
+			'use strict';
+
+			var open = XHR.prototype.open;
+			var send = XHR.prototype.send;
+
+			XHR.prototype.open = function(method, url, async, user, pass) {
+				this._url = url;
+				open.call(this, method, url, async, user, pass);
 			};
-		})(window.XMLHttpRequest.prototype.open);
+
+			XHR.prototype.send = function(data) {
+				var self = this;
+				var oldOnReadyStateChange;
+				var url = this._url;
+
+				function onReadyStateChange() {
+					if(self.readyState == 4 /* complete */) {
+						/* This is where you can put code that you want to execute post-complete*/
+						console.log('onReadyStateChange : ', url);
+					}
+
+					if(oldOnReadyStateChange) {
+						oldOnReadyStateChange();
+					}
+				}
+
+				/* Set xhr.noIntercept to true to disable the interceptor for a particular call */
+				if(!this.noIntercept) {            
+					if(this.addEventListener) {
+						this.addEventListener("readystatechange", onReadyStateChange, false);
+					} else {
+						oldOnReadyStateChange = this.onreadystatechange; 
+						this.onreadystatechange = onReadyStateChange;
+					}
+				}
+				send.call(this, data);
+			}
+		})(XMLHttpRequest);
 	}
 })();
