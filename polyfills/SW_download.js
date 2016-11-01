57437d76fe263e5c527614fa472c5af6becd2f7b
bakuzan <imannoyed4days@hotmail.co.uk>
2016-11-01 11:41:41 +0000

    sw_download : refactor.
    - Hoping to fix closure issue.
    - Refactored the end of downloads.

--- polyfills/SW_download.js
+++ polyfills/SW_download.js
@@ -2,7 +2,7 @@
  *  
  *  @description  GM_download replacement with built in zipping.
  *  @author       Bakuzan
- *  @version      1.2
+ *  @version      1.4
  */
 
 // must include "@grant GM_xmlhttpRequest" at userscript metadata block
@@ -14,11 +14,29 @@ if (typeof GM_download !== 'function') {
         throw new Error('GM_xmlhttpRequest is undefined. Please set @grant GM_xmlhttpRequest at metadata block.');
     }
 	
+	function downloadAndFinish(requestData, downloadContent, downloadName) {
+		saveAs(downloadContent, downloadName);
+		if (typeof requestData.onafterload === 'function') requestData.onafterload(); // call onload function
+	}
+	
 	function downloadZipFile(zip, requestData) {
 		zip.generateAsync({type:"blob"}).then(function(content) {
-			saveAs(content, 'idh-multiple-file-download.zip');
-			if (typeof requestData.onafterload === 'function') requestData.onafterload(); // call onload function
+			downloadAndFinish(requestData, content, 'idh-multiple-file-download.zip');
 		});
+	}
+	
+	function addDownloadItemToZip(zip, name) {
+		return function(res) {
+			console.log(name, res, zip);
+			zip.file(name, res.response);
+		}
+	}
+	
+	function initiateDownload(data) {
+		return function(res) {
+			var blob = new Blob([res.response], {type: 'application/octet-stream'});
+			downloadAndFinish(data, blob, data.name);
+		}
 	}
 	
     function SW_download (urls, options) {
@@ -39,37 +57,22 @@ if (typeof GM_download !== 'function') {
 			for(var i = 0, length = urls.length; i < length; i++) {
 				var download = urls[i];
 				if (download.url === null) continue;
-				var url = download.url,
-					name = download.name;
-					
-				data.url = url;
+				
+				var name = download.name;			
+				data.url = download.url;
 				data.name = name;
-				data.onload = function addDownloadItemToZip(zip, name) {
-					return function(res) {
-						console.log(name, res, zip);
-						zip.file(name, res.response);
-					}
-				}(zip, name)
+				data.onload = addDownloadItemToZip(zip, name);
 				GM_xmlhttpRequest(data);
 			}
 			data.onafterload = options.onload; // onload function support
 			downloadZipFile(zip, data);
 		} else {
 			if (urls instanceof Object === false) return;
-
             if (urls.url == null) return;
 			
 			data.name = urls.name;
 			data.url = urls.url;
-			data.onload = function initiateDownload(name) {
-				return function(res) {
-					var blob = new Blob([res.response], {type: 'application/octet-stream'});
-
-					saveAs(blob, name);
-
-					if (typeof data.onafterload === 'function') data.onafterload(); // call onload function
-				}
-			}(data.name)
+			data.onload = initiateDownload(data);
 			data.onafterload = options.onload; // onload function support
 			
 			return GM_xmlhttpRequest(data);
