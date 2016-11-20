ef7cec4be7fabe304575635b2f61f75180624a87
bakuzan <imannoyed4days@hotmail.co.uk>
2016-11-20 22:59:20 +0000

    added encode hash

---
+++ encode-hash.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Encode hash in url
+// @namespace    http://github.com/bakuzan/user-scripts
+// @version      0.0.1
+// @description  Fix non-encoded urls when opened from windows explorer.
+// @author       Bakuzan
+// @include      file:///*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+	
+	var fileUrl = window.location.href;
+	if (fileUrl.indexOf('#') > -1) {
+		window.location.href = fileUrl.replace(/#/g, '%23');
+	}
+	
+})();
