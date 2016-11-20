e3d9d4278e5cc1e5f6f6e1ec75265bb379f5655a
bakuzan <imannoyed4days@hotmail.co.uk>
2016-11-20 11:37:45 +0000

    ontv : exclude webmshare.com

--- open-new-tab-video/open-new-tab-video.user.js
+++ open-new-tab-video/open-new-tab-video.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Open new tab video
 // @namespace    http://github.com/bakuzan/user-scripts
-// @version      0.0.33
+// @version      0.0.34
 // @description  Allow you to open a video in a new tab.
 // @author       Bakuzan
 // @include      http*
 // @exclude      http://localhost:*
 // @exclude		 *://gfycat.com/*
 // @exclude      *.mixtape.moe/*
+// @exclude      http://webmshare.com/*
 // @exclude		 *googlevideo.com*
 // @require		 https://raw.githubusercontent.com/bakuzan/useful-code/master/scripts/wrapElementWithNewParent.js
 // @require		 https://raw.githubusercontent.com/bakuzan/useful-code/master/scripts/buildElement.js
