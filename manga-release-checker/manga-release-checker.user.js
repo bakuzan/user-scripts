1d3444df04d34287c49ea4efa8ba90584891af1d
bakuzan <imannoyed4days@hotmail.co.uk>
2016-12-02 11:24:34 +0000

    mrc : rework to allow multiple site support.
    Made similar to how arh was converted when allowing kissanime support.

--- manga-release-checker/manga-release-checker.user.js
+++ manga-release-checker/manga-release-checker.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Manga Release Checker.
 // @namespace    https://github.com/bakuzan/user-scripts/tree/master/manga-release-checker
-// @version      0.2.3
-// @description  Pull out manga latest releases that are in my mal reading list. [supported sites: mangafox]
+// @version      0.3.0
+// @description  Pull out manga latest releases that are in my mal reading list. [supported sites: mangafox, eatmanga]
 // @author       Bakuzan
-// @match		 http://mangafox.me/releases/*
-// @incude       http://mangafox.me/releases/*
+// @include		 http://mangafox.me/releases/*
+// @include      http://eatmanga.com/latest/*
 // @resource     stylesheet https://raw.githubusercontent.com/bakuzan/user-scripts/master/manga-release-checker/manga-release-checker.css
 // @require		 https://raw.githubusercontent.com/bakuzan/useful-code/master/scripts/buildElement.js
 // @grant        GM_addStyle
@@ -19,15 +19,14 @@
 	var cssTxt  = GM_getResourceText ("stylesheet");
 	GM_addStyle (cssTxt);
 	
-    var readingList = [],
+    var CONTAINER_ID = 'userscript-mrc-container',
+        processors = {
+            mangafox: mangafoxProcessor,
+            eatmanga: eatmangaProcessor
+        },
+        readingList = [],
 		REGEX = /\W/g,
-		content = document.getElementById('content'),
-		nav = document.getElementById('nav'),
-		releaseList = document.getElementById('updates'),
-        releases = releaseList.children,
-		len = releases.length,
-        newChapterContainer = buildElement('DIV', { id: 'userscript-mrc-container' });
-	newChapterContainer.appendChild(nav);
+        REGEX_EXTRACTER = /([w]{3}([.]))|(([.])\w{2})|([.com]$)/g;
 	
 	function cleanText(text) {
 		return text.toLowerCase().replace(REGEX, '');
@@ -41,41 +40,68 @@
         }
 		return itemLowerCase.replace(REGEX, '');
 	}
-	
-	GM_xmlhttpRequest({
-		method: "GET",
-		url: "https://raw.githubusercontent.com/bakuzan/user-scripts/master/manga-release-checker/manga-spellings.json",
-		onload: function(response) {
-			var data = JSON.parse(response.responseText),
-            series = data.names;
-			for(var i = 0, len = series.length; i < len; i++) {
-				readingList.push(cleanText(series[i]));
-			}
-		}
-	});
-	
-    GM_xmlhttpRequest({
-        method: "GET",
-        url: "http://myanimelist.net/malappinfo.php?u=bakuzan&status=all&type=manga",
-        onload: function(response) {
-            var xml = response.responseXML,
-				        nodes = xml.evaluate("//myanimelist/manga[my_status=1]/series_title/text()", xml, null, XPathResult.ANY_TYPE, null),
-				        result = nodes.iterateNext();
-            while (result) {
-                readingList.push(cleanText(result.nodeValue));
-                result = nodes.iterateNext();
+    
+    function eatmangaProcessor() {
+        
+    }
+    
+    function mangafoxProcessor() {
+        var content = document.getElementById('content'),
+            nav = document.getElementById('nav'),
+            releaseList = document.getElementById('updates'),
+            releases = releaseList.children,
+            len = releases.length,
+            newChapterContainer = buildElement('DIV', { id: CONTAINER_ID });
+
+        while (len--) {
+            var newChapter = releases[len],
+                text = newChapter.getElementsByTagName('a')[0].textContent;
+            if(readingList.indexOf(processText(text)) > -1) {
+                newChapter.className += ' userscript-mrc-highlight';
+                newChapterContainer.insertBefore(newChapter, nav.nextSibling);
             }
-					  var len = releases.length;
-					
-            while (len--) {
-                var newChapter = releases[len],
-					          text = newChapter.getElementsByTagName('a')[0].textContent;
-                if(readingList.indexOf(processText(text)) > -1) {
-                    newChapter.className += ' userscript-mrc-highlight';
-                    newChapterContainer.insertBefore(newChapter, nav.nextSibling);
-                }
-            }
-            content.insertBefore(newChapterContainer, releaseList);
         }
-    });
+        
+        newChapterContainer.appendChild(nav);
+        content.insertBefore(newChapterContainer, releaseList);
+    }
+    
+    function getProcessor() {
+        var host = window.location.host;
+        return host.replace(REGEX_EXTRACTER, '');
+    }
+
+    function extractReleases(response) {
+        var xml = response.responseXML,
+            nodes = xml.evaluate("//myanimelist/manga[my_status=1]/series_title/text()", xml, null, XPathResult.ANY_TYPE, null),
+            result = nodes.iterateNext();
+        while (result) {
+            readingList.push(cleanText(result.nodeValue));
+            result = nodes.iterateNext();
+        }
+        processors[`${getProcessor()}`]();
+    }
+    
+    function getExceptions(response) {
+        var data = JSON.parse(response.responseText),
+            series = data.names;
+        for(var i = 0, len = series.length; i < len; i++) {
+            readingList.push(cleanText(series[i]));
+        }
+        
+        GM_xmlhttpRequest({
+            method: "GET",
+            url: "http://myanimelist.net/malappinfo.php?u=bakuzan&status=all&type=manga",
+            onload: extractReleases
+        });
+    }
+    
+    (function () {
+        GM_xmlhttpRequest({
+            method: "GET",
+            url: "https://raw.githubusercontent.com/bakuzan/user-scripts/master/manga-release-checker/manga-spellings.json",
+            onload: getExceptions
+        });
+    })();
+
 })();
