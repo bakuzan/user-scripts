0228332cf853185c4ad87698759a1f8b7b6bdcf7
bakuzan <imannoyed4days@hotmail.co.uk>
2019-03-25 21:01:47 +0000

    improve downloader with loading screen.

--- polyfills/SW_download.js
+++ polyfills/SW_download.js
@@ -2,12 +2,13 @@
  *  
  *  @description  GM_download replacement with built in zipping.
  *  @author       Bakuzan
- *  @version      2.0
+ *  @version      3.0.0
  */
 
 // must include "@grant GM_xmlhttpRequest" at userscript metadata block
 // must include "@require FileSaver" at userscript metadata block
 // must include "@require jszip" at userscript metadata block
+// must include "@require bakuzan/useful-code/master/scripts/buildElement.js" at userscript metadata block
 
 if (typeof GM_download !== 'function') {
     if (typeof GM_xmlhttpRequest !== 'function') {
@@ -19,10 +20,14 @@ if (typeof GM_download !== 'function') {
 		if (typeof options.onload === 'function') options.onload(); // call onload function
 	}
 	
-	function downloadZipFile(zip, options) {
+	function downloadZipFile(zip, options, loadingDriver) {
 		zip.generateAsync({type:"blob"}).then(function(content) {
-			if(content.size === 22) alert('The zip is empty.');
-			else downloadAndFinish(options, content, 'idh-multiple-file-download.zip');
+			loadingDriver.remove();
+			if(content.size === 22) {
+				alert('The zip is empty.');
+			} else {
+				downloadAndFinish(options, content, 'idh-multiple-file-download.zip');
+			}
 		});
 	}
 	
@@ -30,6 +35,49 @@ if (typeof GM_download !== 'function') {
 		var arraybuffer = result.response;
         zip.file(name, arraybuffer, { binary: true });
 		return arraybuffer;
+	}
+	
+	function startLoadingDisplay(total) {
+		const display = buildElement('div', {
+          id: 'userscript-idh-loading',
+          textContent: 'Downloading...'
+        }),
+		progressLoad = buildElement('div', {
+          id: 'userscript-idh-progressLoad',
+          textContent: `0/${total} Queued`
+        }),
+		progressDone = buildElement('div', {
+          id: 'userscript-idh-progressDone',
+          textContent: `0/${total} Loaded`
+        });
+		
+		display.appendChild(progressLoad)
+		display.appendChild(progressDone)
+		document.body.appendChild(display);
+		
+		const driver = {
+			__total: total,
+			__loading: 0,
+			__finished: 0,
+			setLoading(num) {
+				this.__loading = num;
+				progressLoad.textContent = `${num}/${this.__total} Queued`;
+			},
+			setLoaded(num){
+				this.__finished = num;
+				progressDone.textContent = `${num}/${this.__total} Loaded`;
+			},
+			zipping() {
+				progressLoad.textContent = '';
+				progressDone.textContent = '';
+				display.textContent = 'Zipping...';
+			},
+			remove() {
+				document.body.removeChild(display);
+			}
+		};
+		
+		return driver;
 	}
 	
     function SW_download (urls, options) {
@@ -46,11 +94,16 @@ if (typeof GM_download !== 'function') {
 		}
 		
 		if(Object.prototype.toString.call(urls) === '[object Array]' ) {
+			const loadingDriver = startLoadingDisplay(urls.length);
 			var zip = new JSZip(),
 				promises = [];
+				
 			for(let i = 0, length = urls.length; i < length; i++) {
 				let download = urls[i];
-				if (download.url === null) continue;
+				if (download.url === null) {
+					continue;
+				}
+				
 				var promise = new Promise(function(resolve, reject) { 
 					GM_xmlhttpRequest({
 						method: 'GET',
@@ -58,15 +111,19 @@ if (typeof GM_download !== 'function') {
 						url: download.url,
 						name: download.name,
 						onload: function(result) {
+							loadingDriver.setLoaded(i+1);
 							resolve(getDataForZipping(result, zip, download.name));
 						}
 					});
 				});
+				
 				promises.push(promise);
+				loadingDriver.setLoading(i+1);
 			}
 			//Pretty sweet way to force zipping process to wait until data has been retrieved.
 			Promise.all(promises).then(function(values) {
-				downloadZipFile(zip, options);
+				loadingDriver.zipping();
+				downloadZipFile(zip, options, loadingDriver);
 			});
 		} else {
 			if (urls instanceof Object === false) return;
