205fa7d78343902bfd0292eee0b9b50d756dbcf9
bakuzan <imannoyed4days@hotmail.co.uk>
2021-05-04 10:28:28 +0100

    Filter lists on people page for mal improvements.

--- voice-actor-filter.user.js
+++ voice-actor-filter.user.js
@@ -1,65 +1,108 @@
 // ==UserScript==
 // @name         MAL Voice role filter
 // @namespace    https://github.com/bakuzan/user-scripts
-// @version      0.1.1
+// @version      0.2.0
 // @description  Filter MAL voice actor roles by your MAL list anime.
 // @author       Bakuzan
 // @match        http://myanimelist.net/people/*
-// @include		 https://myanimelist.net/people/*
+// @include		   https://myanimelist.net/people/*
 // @require      https://raw.githubusercontent.com/bakuzan/useful-code/master/scripts/buildElement.js
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    var ROLE_FILTER_CONTROL = ' role-filter-control';
-    var ADD_ID = 'hide-add-checkbox';
-    var EDIT_ID = 'hide-edit-checkbox';
-    var ADD_TEXT = 'button_add';
-    var EDIT_TEXT = 'button_edit';
-    var HIDE_ADD_TEXT = 'Hide unlisted anime.';
-    var HIDE_EDIT_TEXT = 'Hide my anime.';
-    
-    //Create filter controls to add to page.
-    var checkboxContainer = buildElement('div', { id: 'voice-actor-filter-controls', className: ' borderClass' });
-    var hideAddCheckBox = buildElement('input', { id: ADD_ID, type: 'checkbox', className: ROLE_FILTER_CONTROL });
-    var hideAddText = buildElement('span', { textContent: HIDE_ADD_TEXT });
-    var hideEditCheckBox = buildElement('input', { id: EDIT_ID, type: 'checkbox', className: ROLE_FILTER_CONTROL });
-    var hideEditText = buildElement('span', { textContent: HIDE_EDIT_TEXT });
-    
+(function () {
+  'use strict';
+
+  const ROLE_FILTER_CONTROL = ' role-filter-control';
+  const ADD_ID = 'hide-add-checkbox';
+  const EDIT_ID = 'hide-edit-checkbox';
+  const ADD_TEXT = 'button_add';
+  const EDIT_TEXT = 'button_edit';
+  const HIDE_ADD_TEXT = 'Hide unlisted series.';
+  const HIDE_EDIT_TEXT = 'Hide listed series.';
+  const IGNORE_TABLES_FROM_HERE_CLASS = 'bgColor1';
+
+  function getRolesTables() {
+    const tables = Array.from(
+      document.querySelectorAll('table td:nth-child(2) table')
+    );
+
+    const cutoff = tables.findIndex(
+      (x) => x.className == IGNORE_TABLES_FROM_HERE_CLASS
+    );
+
+    return tables.slice(0, cutoff);
+  }
+
+  function getRolesLists() {
+    return getRolesTables().map((x) =>
+      Array.from(x.querySelector('tbody').getElementsByTagName('tr'))
+    );
+  }
+
+  function checkRoles(roleListIndex, text, hideIt) {
+    const roles = getRolesLists()[roleListIndex];
+
+    for (let i = 0, len = roles.length; i < len; i++) {
+      const role = roles[i];
+      const anchors = role.childNodes[3].getElementsByTagName('a');
+      const type = anchors[1].className;
+
+      if (type.indexOf(text) > -1) {
+        role.style.display = hideIt ? 'none' : '';
+      }
+    }
+  }
+
+  function changeHandler(event) {
+    const target = event.target;
+    const [targetId, num] = target.id.split('_');
+
+    if (targetId === ADD_ID) return checkRoles(num, ADD_TEXT, target.checked);
+    if (targetId === EDIT_ID) return checkRoles(num, EDIT_TEXT, target.checked);
+  }
+
+  function createControls(num) {
+    const checkboxContainer = buildElement('div', {
+      id: `voice-actor-filter-controls_${num}`,
+      className: ' borderClass'
+    });
+
+    const hideAddCheckBox = buildElement('input', {
+      id: `${ADD_ID}_${num}`,
+      type: 'checkbox',
+      className: ROLE_FILTER_CONTROL
+    });
+
+    const hideEditCheckBox = buildElement('input', {
+      id: `${EDIT_ID}_${num}`,
+      type: 'checkbox',
+      className: ROLE_FILTER_CONTROL
+    });
+
+    const hideAddText = buildElement('span', { textContent: HIDE_ADD_TEXT });
+    const hideEditText = buildElement('span', { textContent: HIDE_EDIT_TEXT });
+
     hideAddCheckBox.addEventListener('change', changeHandler);
     hideEditCheckBox.addEventListener('change', changeHandler);
-    
+
     checkboxContainer.appendChild(hideAddCheckBox);
     checkboxContainer.appendChild(hideAddText);
     checkboxContainer.appendChild(hideEditCheckBox);
     checkboxContainer.appendChild(hideEditText);
-    
-    //Get roles list tr's
-    var content = document.getElementById('content');
-    var tbodys = content.getElementsByTagName('tbody');
-    var rolesList = tbodys[1];
-    var roles = rolesList.getElementsByTagName('tr');
 
-    //Add filter controls to page.
-    tbodys[0].childNodes[0].childNodes[2].insertBefore(checkboxContainer, rolesList.parentNode);
+    return checkboxContainer;
+  }
 
-    //Check each role's class name.
-    function checkRoles(text, hideIt) {
-      for(var i = 0, len = roles.length; i < len; i++) {
-        var role = roles[i];
-        var anchors = role.childNodes[3].getElementsByTagName('a');
-        var type = anchors[1].className;
-        if(type.indexOf(text) > -1) {
-            role.style.display = hideIt ? 'none' : '';
-        }
-      }
-    }
-    
-    function changeHandler(event) {
-        var target = event.target;
-        if(target.id === ADD_ID) return checkRoles(ADD_TEXT, target.checked);
-        if(target.id === EDIT_ID) return checkRoles(EDIT_TEXT, target.checked);
-    }   
-    
+  function setup() {
+    const pageContainer = document.querySelector('table td:nth-child(2)');
+    const tables = getRolesTables();
+
+    tables.forEach((x, i) => {
+      const controls = createControls(i);
+      pageContainer.insertBefore(controls, x);
+    });
+  }
+
+  setup();
 })();
