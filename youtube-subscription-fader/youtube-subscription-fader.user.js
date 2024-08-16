1a3cccd6d02d49e5b319e6e7ec146639dbcb83b8
bakuzan <imannoyed4days@hotmail.co.uk>
2024-08-16 15:22:38 +0100

    Update youtube-subscription-fader.user.js
    Fixes required for the script to work 16 August 2024; I expect this to break whenever youtube make changes.

--- youtube-subscription-fader/youtube-subscription-fader.user.js
+++ youtube-subscription-fader/youtube-subscription-fader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube subscription fader
 // @namespace    http://github.com/bakuzan/user-scripts
-// @version      0.2.2
+// @version      0.2.4
 // @description  Fade out watched videos in subscriptions
 // @author       Bakuzan
 // @noframes
@@ -11,69 +11,71 @@
 // ==/UserScript==
 
 (function () {
-  'use strict';
+    'use strict';
 
-  const containerSelector =
-    'ytd-section-list-renderer[page-subtype="subscriptions"]';
-  const listItemSelector = 'ytd-shelf-renderer.ytd-item-section-renderer';
-  const gridItemSelector = 'ytd-grid-video-renderer.ytd-grid-renderer';
-  const progressSelector = '.ytd-thumbnail-overlay-resume-playback-renderer';
+    const listItemSelector = 'ytd-rich-grid-media';
+    const progressSelector = '.ytd-thumbnail-overlay-resume-playback-renderer';
 
-  const createItemGetter = (element) => (selector) =>
+    const createItemGetter = (element) => (selector) =>
     element ? Array.from(element.querySelectorAll(selector)) : [];
 
-  function runScript() {
-    let timer = 0;
-    const container = document.querySelector(containerSelector);
-    const getSubscriptions = createItemGetter(container);
+    const matchNodesOfInterest = (x) =>
+    x.tagName == 'YTD-THUMBNAIL-OVERLAY-NOW-PLAYING-RENDERER';
 
-    function fadeSubscriptions() {
-      let items = getSubscriptions(gridItemSelector);
-
-      if (!items.length) {
-        items = getSubscriptions(listItemSelector);
-      }
-
-      for (const item of items) {
-        const prog = item.querySelector(progressSelector);
-        const width =
-          prog && prog.style && prog.style.width
-            ? Number(prog.style.width.slice(0, -1))
-            : 0;
-
-        // Fade out the item if have watched more than 90% of it.
-        if (width > 90) {
-          item.style = `opacity: 0.25;`;
-        } else {
-          item.style = `opacity: 1;`;
-        }
-      }
+    function debug(...other) {
+        console.log(`[Youtube Subscription Fader] :: `, ...other);
     }
 
-    const obs = new MutationObserver(([entry]) => {
-      const hasAdded = entry.addedNodes.length;
-      const hasRemoved = entry.removedNodes.length;
+    function runScript() {
+        let timer = 0;
+        const container = document.documentElement;
+        const getSubscriptions = createItemGetter(container);
 
-      if (hasAdded || hasRemoved) {
-        clearTimeout(timer);
-        timer = window.setTimeout(fadeSubscriptions, 500);
-      }
-    });
+        function fadeSubscriptions() {
+            let items = getSubscriptions(listItemSelector);
+            debug(`Fading Subscriptions? :: `, items);
+            for (const item of items) {
+                const prog = item.querySelector(progressSelector);
+                const width =
+                      prog && prog.style && prog.style.width
+                ? Number(prog.style.width.slice(0, -1))
+                : 0;
+                debug('Fading? :: ', item, prog, width);
+                // Fade out the item if have watched more than X% of it.
+                const opacity = width > 90 ? 0.10 : (100 - width) / 100;
+                item.style = `opacity: ${opacity};`;
+            }
+        }
 
-    obs.observe(container, { childList: true, subtree: true });
-    requestAnimationFrame(fadeSubscriptions);
+        const obs = new MutationObserver(([entry]) => {
+            const hasAdded = Array.from(entry.addedNodes).some(matchNodesOfInterest);
+            const hasRemoved = Array.from(entry.removedNodes).some(
+                matchNodesOfInterest
+            );
 
-    return () => obs.disconnect();
-  }
+            if (hasAdded || hasRemoved) {
+                clearTimeout(timer);
+                timer = window.setTimeout(fadeSubscriptions, 500);
+            }
+        });
 
-  // Start script
-  const urlMatch = /https:\/\/.*youtube.com\/feed\/subscriptions.*/;
+        obs.observe(document.documentElement, {
+            attributes: false,
+            childList: true,
+            subtree: true
+        });
 
-  if (document && document.readyState !== 'loading') {
-    monitorUrlChanges(urlMatch, runScript);
-  } else {
-    window.addEventListener('DOMContentLoaded', () =>
-      monitorUrlChanges(urlMatch, runScript)
-    );
-  }
+        return () =>  obs.disconnect();
+    }
+
+    // Start script
+    const urlMatch = /https:\/\/.*youtube.com\/feed\/subscriptions.*/;
+
+    if (document && document.readyState !== 'loading') {
+        monitorUrlChanges(urlMatch, runScript);
+    } else {
+        window.addEventListener('DOMContentLoaded', () =>
+                                monitorUrlChanges(urlMatch, runScript)
+                               );
+    }
 })();
